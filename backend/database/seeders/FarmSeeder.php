da557b34aa34e9111393b86dacd303812f15a3dd
gabrieltarozzo <gabriel.tarozzo@hotmail.com>
2025-08-25 04:13:54 -0300

    docker config

--- backend/database/seeders/FarmSeeder.php
+++ backend/database/seeders/FarmSeeder.php
@@ -27,8 +27,53 @@ class FarmSeeder extends Seeder
             'area_ha' => 200,
             'lat' => -15.7801,
             'lng' => -47.9292,
+            'city' => 'Brasilia',
+            'state' => 'DF',
+        ]);
+
+        Farm::create([
+            'name' => 'Fazenda Lago Sul',
+            'owner' => 'Carlos Pereira',
+            'crop' => 'Soja',
+            'area_ha' => 120,
+            'lat' => -15.7825,
+            'lng' => -47.9350,
+            'city' => 'Brasilia',
+            'state' => 'DF',
+        ]);
+
+        Farm::create([
+            'name' => 'Fazenda Ipê Amarelo',
+            'owner' => 'Ana Santos',
+            'crop' => 'Milho',
+            'area_ha' => 180,
+            'lat' => -15.7778,
+            'lng' => -47.9210,
+            'city' => 'Brasilia',
+            'state' => 'DF',
+        ]);
+
+        Farm::create([
+            'name' => 'Fazenda Buriti',
+            'owner' => 'Rafael Costa',
+            'crop' => 'Algodão',
+            'area_ha' => 90,
+            'lat' => -15.7852,
+            'lng' => -47.9270,
             'city' => 'Brasília',
             'state' => 'DF',
         ]);
+
+        Farm::create([
+            'name' => 'Fazenda Veredas',
+            'owner' => 'Beatriz Lima',
+            'crop' => 'Feijão',
+            'area_ha' => 140,
+            'lat' => -15.7745,
+            'lng' => -47.9348,
+            'city' => 'Brasília',
+            'state' => 'DF',
+        ]);
+    
     }
 }
